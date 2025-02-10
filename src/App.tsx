723e56b2619e8890e0bd671e02af750f1335f7cb
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-10 17:08:11 +0300

    fix: :lipstick: desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import ModelForm from '@/components/ModelForm';
-import { Layout } from 'antd';
+import { Col, Layout, Row } from 'antd';
 import ModelView from '@/components/ModelView';
 
 function App() {
   return (
-    <Layout>
-      <ModelForm></ModelForm>
-      <ModelView></ModelView>
+    <Layout style={{ height: '100vh', padding: '0 16px', justifyContent: 'start' }}>
+      <Row gutter={[16, 8]} style={{ height: '100%' }} wrap={false}>
+        <Col style={{ alignSelf: 'center' }}>
+          <ModelForm
+            initialValues={{
+              height: 200,
+              width: 100,
+              length: 150,
+            }}
+            onSubmit={(values) => console.log(values)}
+          />
+        </Col>
+        <Col flex={1}>
+          <ModelView />
+        </Col>
+      </Row>
     </Layout>
   );
 }
