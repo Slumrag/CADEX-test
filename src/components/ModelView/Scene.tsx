803fb495bdff27c06be0af6e099645a9139b0ec5
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-11 20:06:39 +0300

    refactor: remove cameraHelper

--- src/components/ModelView/Scene.tsx
+++ src/components/ModelView/Scene.tsx
@@ -1,16 +1,15 @@
 import { CameraControls } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
 import React, { FC, PropsWithChildren, useRef } from 'react';
 
 export const Scene: FC<PropsWithChildren> = ({ children }) => {
-  const { camera, scene } = useThree();
+  // const { camera, scene } = useThree();
   const cameraControlsRef = useRef(null);
 
   return (
     <>
       <CameraControls ref={cameraControlsRef} />
       <axesHelper args={[3]}></axesHelper>
-      <cameraHelper args={[camera]}></cameraHelper>
+      {/* <cameraHelper args={[camera]}></cameraHelper> */}
       <ambientLight intensity={Math.PI / 4} />
       <directionalLight position={[1, 1, 1]} />
       <group>{children}</group>
