525b76fe98906b68dfa1a197c4ab986a5d03f07e
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-11 21:41:59 +0300

    feat: move box center to worl origin

--- src/components/ModelView/BufferBox.tsx
+++ src/components/ModelView/BufferBox.tsx
@@ -1,13 +1,37 @@
 import { ThreeElements } from '@react-three/fiber';
-import React, { FC } from 'react';
-
+import React, { FC, useEffect, useRef } from 'react';
+import * as THREE from 'three';
 export const BufferBox: FC<ThreeElements['mesh'] & { points: Float32Array }> = ({
   points,
   ...props
 }) => {
+  const ref = useRef<THREE.Group>(null!);
+  useEffect(() => {
+    if (ref.current) {
+      const group = ref.current;
+      const groupCenter = new THREE.Vector3();
+      const currentPos = new THREE.Vector3();
+      const worldOrigin = new THREE.Vector3(0, 0, 0);
+
+      const mesh = group.getObjectByName('box') as THREE.Mesh;
+      mesh?.geometry.computeVertexNormals();
+      mesh?.geometry.computeBoundingBox();
+      mesh?.geometry.boundingBox?.getCenter(groupCenter);
+      group.getWorldPosition(currentPos);
+      currentPos.add(groupCenter);
+      const distToOrigin = worldOrigin.distanceTo(currentPos);
+
+      if (distToOrigin > 1e-2) {
+        group.translateX(-currentPos.x);
+        group.translateY(-currentPos.y);
+        group.translateZ(-currentPos.z);
+      }
+    }
+  }, [points]);
+
   return (
-    <>
-      <mesh {...props}>
+    <group ref={ref}>
+      <mesh name={'box'} {...props}>
         <bufferGeometry>
           <bufferAttribute attach='attributes-position' args={[points, 3]} />
         </bufferGeometry>
@@ -19,6 +43,6 @@ export const BufferBox: FC<ThreeElements['mesh'] & { points: Float32Array }> = (
         </bufferGeometry>
         <meshStandardMaterial color={'black'} wireframe />
       </mesh>
-    </>
+    </group>
   );
 };
