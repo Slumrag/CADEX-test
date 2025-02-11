7f167d202a5c00639374c1e8869b06d54b89f567
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-11 20:07:38 +0300

    feat: add wireframe to box

--- src/components/ModelView/BufferBox.tsx
+++ src/components/ModelView/BufferBox.tsx
@@ -1,24 +1,24 @@
 import { ThreeElements } from '@react-three/fiber';
-import React, { FC, useRef } from 'react';
-import * as THREE from 'three';
+import React, { FC } from 'react';
 
 export const BufferBox: FC<ThreeElements['mesh'] & { points: Float32Array }> = ({
   points,
   ...props
 }) => {
-  const ref = useRef<THREE.Mesh>(null!);
-  // console.log(ref.current);
-  // useHelper(ref, VertexNormalsHelper);
   return (
-    <mesh ref={ref} {...props}>
-      <bufferGeometry>
-        <bufferAttribute attach='attributes-position' args={[points, 3]} />
-        {/* <bufferAttribute attach='index' args={[new Uint16Array(indices), 3]} /> */}
-      </bufferGeometry>
-      {/* <boxGeometry args={[1, 1.2, 1.3]}></boxGeometry> */}
-      <meshStandardMaterial color={'green'} />
-      {/* <Wireframe polygonOffset={true} polygonOffsetFactor={1} polygonOffsetUnits={1} /> */}
-      {/* <Helper type={VertexNormalsHelper} /> */}
-    </mesh>
+    <>
+      <mesh {...props}>
+        <bufferGeometry>
+          <bufferAttribute attach='attributes-position' args={[points, 3]} />
+        </bufferGeometry>
+        <meshStandardMaterial color={'darkgrey'} />
+      </mesh>
+      <mesh {...props}>
+        <bufferGeometry>
+          <bufferAttribute attach='attributes-position' args={[points, 3]} />
+        </bufferGeometry>
+        <meshStandardMaterial color={'black'} wireframe />
+      </mesh>
+    </>
   );
 };
