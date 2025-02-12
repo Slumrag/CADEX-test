5f161f15e00f19152ec0bc23a2c6dc984082c99f
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-12 14:11:30 +0300

    feat: :sparkles: impliment camera auto fit to object

--- src/components/ModelView/Scene.tsx
+++ src/components/ModelView/Scene.tsx
@@ -1,9 +1,33 @@
 import { CameraControls } from '@react-three/drei';
-import React, { FC, PropsWithChildren, useRef } from 'react';
+import React, { FC, PropsWithChildren, useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
 export const Scene: FC<PropsWithChildren> = ({ children }) => {
   // const { camera, scene } = useThree();
-  const cameraControlsRef = useRef(null);
+  const cameraControlsRef = useRef<CameraControls>(null!);
+  const childrenRef = useRef<THREE.Group>(null!);
+
+  useEffect(() => {
+    if (!children) {
+      return;
+    }
+    if (cameraControlsRef.current && childrenRef.current) {
+      const children = childrenRef.current;
+      const camera = cameraControlsRef.current;
+      const mesh = children.children[0]?.getObjectByName('box') as THREE.Mesh;
+      console.log(children.children[0]);
+      console.log('mesh', mesh);
+
+      let boundingBox = mesh.geometry.boundingBox;
+      if (boundingBox === null) {
+        mesh.geometry.computeBoundingBox();
+        boundingBox = mesh.geometry.boundingBox as THREE.Box3;
+      }
+      camera.fitToSphere(mesh, true);
+      camera.azimuthAngle = Math.PI / 6;
+      camera.polarAngle = Math.PI / 3;
+    }
+  }, [children]);
 
   return (
     <>
@@ -12,7 +36,7 @@ export const Scene: FC<PropsWithChildren> = ({ children }) => {
       {/* <cameraHelper args={[camera]}></cameraHelper> */}
       <ambientLight intensity={Math.PI / 4} />
       <directionalLight position={[1, 1, 1]} />
-      <group>{children}</group>
+      <group ref={childrenRef}>{children}</group>
     </>
   );
 };
