4642a9c5f4834b772b715087c26771bf4fa1ba99
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-12 21:45:09 +0300

    chore: :wrench: add dev proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tsConfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL + '/api',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
