baebd5c201f2c8ebbd09bfa3e1975cc470dbfc8f
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-12 21:46:03 +0300

    feat: :sparkles: update getBufferGeometry to fetch data from server

--- src/api/getBufferGeometry/getBufferGeometry.ts
+++ src/api/getBufferGeometry/getBufferGeometry.ts
@@ -4,54 +4,16 @@ type Values = {
   length: number;
 };
 
-function delay(ms: number): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => resolve(), ms);
-  });
-}
-
 export async function getBufferGeometry({ height, width, length }: Values) {
-  await delay(1000);
-  const points = [
-    [0, 0, 0], //0
-    [length, 0, 0], //1
-    [length, width, 0], //2
-    [0, width, 0], //3
-    // //top
-    [0, 0, height], //4
-    [length, 0, height], //5
-    [length, width, height], //6
-    [0, width, height], //7
-  ];
-  const getVertex = (indexes: number[]) => {
-    const vertex: number[][] = [];
-    indexes.forEach((el) => {
-      if (points[el]) {
-        vertex.push(points[el]);
-      }
-    });
-    return vertex;
-  };
-  const vertices = [
-    // bottom
-    ...getVertex([3, 1, 0]), //v0
-    ...getVertex([3, 2, 1]), //v1
-    // top
-    ...getVertex([4, 5, 6]), //v2
-    ...getVertex([4, 6, 7]), //v3
-    // forward
-    ...getVertex([0, 4, 7]), // v4
-    ...getVertex([0, 7, 3]), // v5
-    // back
-    ...getVertex([1, 2, 5]), // v6
-    ...getVertex([5, 2, 6]), // v7
-    // right
-    ...getVertex([3, 6, 2]), // v8
-    ...getVertex([6, 3, 7]), // v9
-    // left
-    ...getVertex([1, 4, 0]), // v10
-    ...getVertex([1, 5, 4]), // v11
-  ];
+  try {
+    const url = '/api/calculateBox';
 
-  return vertices.reduce((acc, cur) => [...acc, ...cur]);
+    const params = `?length=${length}&height=${height}&width=${width}`;
+
+    const res = await fetch(url + params, { method: 'GET' });
+    const result = res.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
